1ffaeef0f27722f64e95ee223b049ae4d164d4e7
unknown <PC3SB-2@pc3sb-2.USB.LOCAL>
2014-09-04 19:46:31 -0500

    declaracion e implementacion clase ship

--- sourceCode/spaceWars/ship.cpp
+++ sourceCode/spaceWars/ship.cpp
@@ -1,5 +1,8 @@
 #include "ship.h"
 #include "consoleTools/basicGraphic/basicgraphic.h"
+const float ship::_speed_step=0.0005; //estas unidades son caracter/milisegundo
+const float ship::_speedMax=0.05;
+const float ship::_speedMin= 0;
 
 void ship::initGraphicObject(point pos, float speed, char dir, int flR, int flL, int flT, int flB)
 {
@@ -32,7 +35,7 @@ void ship::initGraphicObject(point pos, float speed, char dir, int flR, int flL,
     _graphDown.append(graphicChar(point(1,0),'V',BG_CYAN));
     _graphDown.append(graphicChar(point(2,0),'_',BG_GREEN));
     _graphDown.append(graphicChar(point(0,1),'\\',BG_GREEN));
-    _graphDown.append(graphicChar(point(1,1),'X',BG_CYAN));
+    _graphDown.append(graphicChar(point(1,1),'X',BG_RED));
     _graphDown.append(graphicChar(point(2,1),'/',BG_GREEN));
     _graphDown.append(graphicChar(point(0,2),'|',BG_GREEN));
     _graphDown.append(graphicChar(point(2,2),'|',BG_GREEN));
@@ -40,18 +43,26 @@ void ship::initGraphicObject(point pos, float speed, char dir, int flR, int flL,
 
 
     _graphUp.clear();
-    _graphUp.append(graphicChar(point(0,3),'_',BG_GREEN));
+    _graphUp.append(graphicChar(point(0,3),'-',BG_GREEN));
     _graphUp.append(graphicChar(point(1,3),'^',BG_CYAN));
-    _graphUp.append(graphicChar(point(2,3),'_',BG_GREEN));
-    _graphUp.append(graphicChar(point(0,2),'\\',BG_GREEN));
-    _graphUp.append(graphicChar(point(1,2),'X',BG_CYAN));
-    _graphUp.append(graphicChar(point(2,2),'/',BG_GREEN));
+    _graphUp.append(graphicChar(point(2,3),'-',BG_GREEN));
+    _graphUp.append(graphicChar(point(0,2),'/',BG_GREEN));
+    _graphUp.append(graphicChar(point(1,2),'X',BG_RED));
+    _graphUp.append(graphicChar(point(2,2),'\\',BG_GREEN));
     _graphUp.append(graphicChar(point(0,1),'|',BG_GREEN));
     _graphUp.append(graphicChar(point(2,1),'|',BG_GREEN));
     _graphUp.append(graphicChar(point(1,0),'^',BG_GREEN));
 
     fillHitArea();
     _hitPower=2;
+
+    _sUp = 0;
+    _sDown = 0;
+    _turnLeft = 0;
+    _turnRight = 0;
+    _fire1 = 0;
+    _fire2 = 0;
+
 }
 
 ship::ship()
@@ -63,6 +74,115 @@ ship::ship(point pos, float speed, char dir, int flR, int flL, int flT, int flB)
 {
     initGraphicObject(pos,speed,dir,flR,flL,flT,flB);
 }
+
+
+
+void ship::speedUP()
+{
+    _speed += _speed_step;
+
+    if(_speed>_speedMax)//si la velocidad llega al  maximo
+        _speed = _speedMax;
+    _speed += _speed_step;
+}
+
+void ship::speedDown()
+{
+    _speed -= _speed_step;
+    if(_speed < _speedMin)
+        _speed = _speedMin;
+}
+
+void ship::turnRight()
+{
+    switch(_direction)
+    {
+    case DIR_RIGHT:
+        _direction = DIR_DOWN;
+        break;
+    case DIR_DOWN:
+        _direction = DIR_LEFT;
+        break;
+    case DIR_LEFT:
+        _direction = DIR_TOP;
+        break;
+    case DIR_TOP:
+        _direction = DIR_RIGHT;
+        break;
+     }
+
+}
+
+void ship::turnLeft()
+{
+    switch(_direction)
+    {
+    case DIR_RIGHT:
+        _direction = DIR_TOP;
+        break;
+    case DIR_DOWN:
+        _direction = DIR_RIGHT;
+        break;
+    case DIR_LEFT:
+        _direction = DIR_DOWN;
+        break;
+    case DIR_TOP:
+        _direction = DIR_LEFT;
+        break;
+    }
+}
+
+void ship::fire1()//disparo Tipo 1
+{
+
+}
+
+void ship::fire2()//disparo tipo 2
+{
+
+}
+
+
+
+int ship::confCmd(int sUp, int sDown, int turnRight, int turnLeft, int fire1, int fire2)
+{
+    _sUp = sUp;
+    _sDown = sDown;
+    _turnRight = turnRight;
+    _turnLeft = turnLeft;
+    _fire1 = fire1;
+    _fire2 = fire2;
+
+
+}
+int ship::recvCmd(int cmd)
+{
+    if(!(_sUp && _sDown && _turnLeft && _turnRight && _fire1 && _fire2))
+        return -1;
+
+
+    if(cmd == _sUp)
+        speedUP();
+    else if(cmd == _sDown)
+        speedDown();
+    else if(cmd == _turnLeft)
+        turnLeft();
+    else if(cmd == _turnRight)
+        turnRight();
+    else if(cmd == _fire1)
+        fire1();
+    else if(cmd == _fire2)
+        fire2();
+    else
+        return 0;
+
+
+
+    return 1;
+
+
+}
+
 
 
 
--- sourceCode/spaceWars/ship.h
+++ sourceCode/spaceWars/ship.h
@@ -6,9 +6,30 @@ class ship:public graphicObjects
 {
 private:
     void initGraphicObject(point pos, float speed, char dir, int flR, int flL, int flT, int flB);
+    static const float _speed_step;
+    static const float _speedMax;
+    static const float _speedMin;
+    int _sUp,_sDown,_turnRight,_turnLeft,_fire1,_fire2;
+
+
 public:
+
     ship();
     ship(point pos, float speed, char dir, int flR, int flL, int flT, int flB);
+    void speedUP();
+    void speedDown();
+    void turnRight();
+    void turnLeft();
+    void fire1();//disparo 1
+    void fire2();//disparo 2
+    int recvCmd(int cmd);
+    int confCmd(int sUp,int sDown,int turnRight, int turnLeft, int fire1, int fire2 );//Llamar antes de recvCmd
+
+
+
+
+
+
 };
 
 #endif // SHIP_H
